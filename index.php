894f4f39b3c0babd457c62da07de0a0cbac0cf63
Ogoorass <igor.piontek@wp.pl>
2024-01-24 21:45:27 +0100

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script type="text/javascript" src="js/list.js"></script>
+    </head>
+    <body onload="main()">
+        <div class="container">
+
+        </div>
+
+    </body>
+</html>
---
+++ js/list_item.js
@@ -0,0 +1,8 @@
+class item {
+    constructor(name, id){
+        this.name = name;
+        this.id = id;
+    }
+}
+
+export default item;
